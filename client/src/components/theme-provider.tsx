67ff81c67c4d84baf748060054e5201486712683
Muhammad-2002 <muham.cs2002@gmail.com>
2025-08-21 21:38:56 +0500

    fix next.js theme issue

--- client/src/components/theme-provider.tsx
+++ client/src/components/theme-provider.tsx
@@ -1,23 +1,14 @@
 "use client"
 
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import dynamic from "next/dynamic"
 import { type ThemeProviderProps } from "next-themes"
 
+const NextThemesProvider = dynamic(
+  () => import("next-themes").then((mod) => mod.ThemeProvider),
+  { ssr: false }
+)
+
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  const [mounted, setMounted] = React.useState(false)
-
-  React.useEffect(() => {
-    setMounted(true)
-  }, [])
-
-  if (!mounted) {
-    return <>{children}</>
-  }
-
-  return (
-    <NextThemesProvider {...props}>
-      {children}
-    </NextThemesProvider>
-  )
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
